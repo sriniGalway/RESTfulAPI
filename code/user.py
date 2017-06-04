cead86d448c0b27cc691ee85a9f7b1c419ca6e46
Kulshrestha <cherryku@amazon.com>
2017-06-05 00:20:39 +0100

    updated for sqlite

--- code/user.py
+++ code/user.py
@@ -1,5 +1,39 @@
+import sqlite3
+
 class User(object):
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username): #change self -> cls as we aren't using self in the method
+        connection = sqlite3.connect(data.db)
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row) #cls is same as User()
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, id):  # change self -> cls as we aren't using self in the method
+        connection = sqlite3.connect(data.db)
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)  # cls is same as User()
+        else:
+            user = None
+
+        connection.close()
+        return user
